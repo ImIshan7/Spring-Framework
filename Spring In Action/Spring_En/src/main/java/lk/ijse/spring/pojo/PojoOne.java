4313c163c287e2647fa0d6f3df6b38464a2e2782
ImIshan7 <ishandanu999@gmail.com>
2023-09-25 11:44:42 +0100

    Add Web App

--- Spring In Action/Spring_En/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ Spring In Action/Spring_En/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -1,10 +1,36 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PojoOne {
+public class PojoOne implements InitializingBean{
+
+    @Value("${user.name}")
+    private String userName;
+
+    @Value("${os.name")
+    private String osName;
+
     public PojoOne(){
         System.out.println(" 1 Pojo One Instantiated");
     }
+
+    @Autowired
+    Environment env;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        env.getProperty("dbUserName");
+        System.out.println("User Name : "+userName);
+        System.out.println("OS Name : "+osName);
+        String s = env.getProperty("Url");
+
+
+    }
+
+
 }
