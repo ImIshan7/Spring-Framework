8c7ef4fa82edd60864af87c58bdcb8ad7ca64334
ImIshan7 <ishandanu999@gmail.com>
2023-09-30 08:49:27 +0100

    Rest Controller

---
+++ Spring In Action/O10_Controllers/target/O10_Controllers-1.0-SNAPSHOT/item.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hp
+  Date: 9/30/2023
+  Time: 7:05 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Item Manage</title>
+</head>
+<body>
+
+<h1>Item  ${ItemName}</h1>
+
+</body>
+</html>
---
+++ Spring In Action/O11_REST_Controllers/src/main/java/lk/ijse/spring/controller/RestArchController.java
@@ -0,0 +1,18 @@
+package lk.ijse.spring.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class RestArchController {
+
+    @GetMapping
+    public void test(){
+        System.out.println("Test");
+    }
+
+
+}
